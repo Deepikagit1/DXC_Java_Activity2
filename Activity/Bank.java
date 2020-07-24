3eca82078d1e4c4b8845b67ceceae7471222e4c1
deepikagit1 <g.deepu1110@gmail.com>
2020-07-24 14:42:52 +0530

    First Commit

---
+++ Activity/Bank.java
@@ -0,0 +1,17 @@
+package Activity;
+
+
+public interface Bank {
+
+	void deposit(double amount);
+
+	void withdraw(double amount, String type);
+
+	double getBalance();
+
+	void customerDetails(int num);
+
+}
+	
+
+
